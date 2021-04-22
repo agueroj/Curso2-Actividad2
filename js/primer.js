d2ef28cac275995cbf3a9d96f28688582b3ae6f2
agueroj <javieraguero153@gmail.com>
2021-04-22 00:42:39 -0300

    js implementado

---
+++ js/primer.js
@@ -0,0 +1,16 @@
+function enviarMensaje() {
+    alert("se envio el correo satisfactoriamente!")
+}
+function myfunction1() {
+    var exp =document.getElementById("experiencia"); 
+    focusMethod = function getFocus() { 
+    document.getElementById("cuadroExperiencia").focus();
+    }
+}
+
+function myfunction2() {
+    var exp =document.getElementById("contacto");
+    focusMethod = function getFocus() { 
+    document.getElementById("cuadroContacto").focus();
+}
+}
